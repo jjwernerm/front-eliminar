ae9f40ac210208fdd7403f9c1807ff5e647dc8f2
jjwernerm <joannywerner@gmail.com>
2024-08-26 03:17:29 -0400

    commit

--- src/views/BuscarEliminar.jsx
+++ src/views/BuscarEliminar.jsx
@@ -1,20 +1,20 @@
 // BuscarEliminar.jsx
-import { useState, useContext, useEffect } from 'react'; 
+import { useState, useContext, useEffect } from 'react';
 // Importa hooks de React para gestionar el estado y efectos colaterales.
 
-import { ImSpinner9 } from "react-icons/im"; 
+import { ImSpinner9 } from "react-icons/im";
 // Importa un ícono de spinner para mostrar mientras se realiza una acción.
 
-import { MdDeleteForever } from "react-icons/md"; 
+import { MdDeleteForever } from "react-icons/md";
 // Importa un ícono para representar la acción de eliminación.
 
-import Alert from '../componentes/Alert.jsx'; 
+import Alert from '../componentes/Alert.jsx';
 // Importa el componente Alert para mostrar mensajes de error o éxito.
 
-import ModalDialogs from '../componentes/Modal.jsx'; 
+import ModalDialogs from '../componentes/Modal.jsx';
 // Importa el componente ModalDialogs para confirmar la eliminación.
 
-import { DatosContext } from '../context/DatosContext.jsx'; 
+import { DatosContext } from '../context/DatosContext.jsx';
 // Importa el contexto para acceder a datos y funciones compartidos entre componentes.
 
 export default function Consultar() {
@@ -94,82 +94,89 @@ export default function Consultar() {
   };
 
   return (
-    <div className="flex flex-col items-center my-4">
+    <>
+      <a
+        href='https://joannywerner.com/dashboardmern'
+        className="ml-5 underline">
+        Regresar al Dashboard
+      </a>
+      <div className="flex flex-col items-center my-4">
 
-      <form
-        className="border border-gray-100 rounded-xl w-9/12 md:w-3/12 p-4 shadow-md"
-        onSubmit={handleBuscar}
-      >
-        <div className="text-center font-bold">Producto a Eliminar</div>
-        
-        <div className="mt-4">
-          <label htmlFor="input-id" className="text-gray-500">Id Producto <span className='text-red-500'>*</span></label>
-          <input
-            type="number"
-            placeholder="Escribe el id del producto"
-            id="input-id"
-            className="border-gray-100 focus:ring-green-400 bg-gray-100 p-1 rounded-xl w-full outline-none focus:ring-1"
-            value={idproducto}
-            onChange={handleIdProductoChange}
-          />
-        </div>
+        <form
+          className="border border-gray-100 rounded-xl w-9/12 md:w-3/12 p-4 shadow-md"
+          onSubmit={handleBuscar}
+        >
+          <div className="text-center font-bold">Producto a Eliminar</div>
 
-        <button
-          type="submit"
-          className={`
+          <div className="mt-4">
+            <label htmlFor="input-id" className="text-gray-500">Id Producto <span className='text-red-500'>*</span></label>
+            <input
+              type="number"
+              placeholder="Escribe el id del producto"
+              id="input-id"
+              className="border-gray-100 focus:ring-green-400 bg-gray-100 p-1 rounded-xl w-full outline-none focus:ring-1"
+              value={idproducto}
+              onChange={handleIdProductoChange}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className={`
           ${btn ? 'bg-gray-400' : 'bg-red-600 hover:bg-red-500'} w-full py-3 px-10 rounded-xl 
           text-white uppercase font-bold mt-5 flex items-center justify-center
         `}
-          disabled={btn}
-        >
-          {spinner ? (
-            <div className="flex items-center">
-              <ImSpinner9 className="animate-spin h-5 w-5 text-white mr-2" />
-              Buscando...
-            </div>
-          ) : (
-            'Buscar'
-          )}
-        </button>
-        {/* Botón para realizar la búsqueda, que muestra un spinner cuando está en proceso */}
+            disabled={btn}
+          >
+            {spinner ? (
+              <div className="flex items-center">
+                <ImSpinner9 className="animate-spin h-5 w-5 text-white mr-2" />
+                Buscando...
+              </div>
+            ) : (
+              'Buscar'
+            )}
+          </button>
+          {/* Botón para realizar la búsqueda, que muestra un spinner cuando está en proceso */}
 
-        {globalErrorMsg.msg && <Alert props={globalErrorMsg} />}
-        {/* Muestra el componente Alert si hay un mensaje de error global */}
-      </form>
+          {globalErrorMsg.msg && <Alert props={globalErrorMsg} />}
+          {/* Muestra el componente Alert si hay un mensaje de error global */}
+        </form>
 
-      <div className="flex justify-center w-full">
-        <table className="border-separate border-spacing-1 border border-slate-500 rounded w-full mx-1 my-4 md:w-1/2 text-sm md:text-base">
-          <caption className="caption-top font-bold">
-            Producto a Eliminar
-          </caption>
-          <thead>
-            <tr>
-              <th></th>
-              <th className="bg-slate-200 border border-slate-600 rounded">Id Producto</th>
-              <th className="bg-slate-200 border border-slate-600 rounded">Nombre del Producto</th>
-            </tr>
-          </thead>
-          {producto && (
-            <tbody>
+        <div className="flex justify-center w-full">
+          <table className="border-separate border-spacing-1 border border-slate-500 rounded w-full mx-1 my-4 md:w-1/2 text-sm md:text-base">
+            <caption className="caption-top font-bold">
+              Producto a Eliminar
+            </caption>
+            <thead>
               <tr>
-                <td className='flex justify-center text-2xl text-red-600'>
-                  <button
-                    onClick={handleOpenModal}
-                  ><MdDeleteForever /></button>
-                  {/* Botón para abrir el modal de eliminación */}
-                </td>
-                <td>{producto.idproducto}</td>
-                <td>{producto.nombre}</td>
+                <th></th>
+                <th className="bg-slate-200 border border-slate-600 rounded">Id Producto</th>
+                <th className="bg-slate-200 border border-slate-600 rounded">Nombre del Producto</th>
               </tr>
-            </tbody>
-          )}
-        </table>
-      </div>
+            </thead>
+            {producto && (
+              <tbody>
+                <tr>
+                  <td className='flex justify-center text-2xl text-red-600'>
+                    <button
+                      onClick={handleOpenModal}
+                    ><MdDeleteForever /></button>
+                    {/* Botón para abrir el modal de eliminación */}
+                  </td>
+                  <td>{producto.idproducto}</td>
+                  <td>{producto.nombre}</td>
+                </tr>
+              </tbody>
+            )}
+          </table>
+        </div>
 
-      <div>
-        {modal && <ModalDialogs nombre={producto.nombre} idproducto={producto.idproducto} onClose={handleCloseModal} />}
-        {/* Muestra el modal si `modal` es true */}
+        <div>
+          {modal && <ModalDialogs nombre={producto.nombre} idproducto={producto.idproducto} onClose={handleCloseModal} />}
+          {/* Muestra el modal si `modal` es true */}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
